50c39eec4a84d347554aa1f4a76953a444799fdd
Ken Fowler <development@kennyf.ca>
2023-02-12 18:02:07 -0500

    feat: add interpreter class to project

---
+++ src/lox/Interpreter.java
@@ -0,0 +1,109 @@
+package lox;
+
+class Interpreter implements Expression.Visitor<Object> {
+    private Object evaluate(Expression expr) {
+        return expr.accept(this);
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean) object;
+        return true;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) return true;
+        if (a == null) return false;
+
+        return a.equals(b);
+    }
+
+    void interpret(Expression expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+
+        return object.toString();
+    }
+
+
+    @Override
+    public Object visitBinaryExpression(Expression.Binary expression) {
+        Object left = evaluate(expression.left);
+        Object right = evaluate(expression.right);
+
+        switch (expression.operator.type) {
+            case GREATER:
+                return (double) left > (double) right;
+            case GREATER_EQUAL:
+                return (double) left >= (double) right;
+            case LESS:
+                return (double) left < (double) right;
+            case LESS_EQUAL:
+                return (double) left <= (double) right;
+            case MINUS:
+                return (double) left - (double) right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return left + (String) right;
+                }
+                break;
+            case SLASH:
+                return (double) left / (double) right;
+            case STAR:
+                return (double) left * (double) right;
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+
+        }
+
+        // Unreachable.
+        return null;
+    }
+
+    @Override
+    public Object visitGroupingExpression(Expression.Grouping expression) {
+        return evaluate(expression.expression);
+    }
+
+    @Override
+    public Object visitLiteralExpression(Expression.Literal expression) {
+        return expression.value;
+    }
+
+    @Override
+    public Object visitUnaryExpression(Expression.Unary expression) {
+        Object right = evaluate(expression.right);
+
+        switch (expression.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                return -(double) right;
+        }
+
+        // Unreachable.
+        return null;
+    }
+}
