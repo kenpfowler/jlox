bb6e217155cb71592d09c53ea27c85689c25b229
Ken Fowler <development@kennyf.ca>
2023-02-12 17:22:01 -0500

    chore: formatted code

--- src/lox/Scanner.java
+++ src/lox/Scanner.java
@@ -10,23 +10,24 @@ public class Scanner {
 
     static {
         keywords = new HashMap<>();
-        keywords.put("and",    TokenType.AND);
-        keywords.put("class",  TokenType.CLASS);
-        keywords.put("else",   TokenType.ELSE);
-        keywords.put("false",  TokenType.FALSE);
-        keywords.put("for",    TokenType.FOR);
-        keywords.put("fun",    TokenType.FUN);
-        keywords.put("if",     TokenType.IF);
-        keywords.put("nil",    TokenType.NIL);
-        keywords.put("or",     TokenType.OR);
-        keywords.put("print",  TokenType.PRINT);
+        keywords.put("and", TokenType.AND);
+        keywords.put("class", TokenType.CLASS);
+        keywords.put("else", TokenType.ELSE);
+        keywords.put("false", TokenType.FALSE);
+        keywords.put("for", TokenType.FOR);
+        keywords.put("fun", TokenType.FUN);
+        keywords.put("if", TokenType.IF);
+        keywords.put("nil", TokenType.NIL);
+        keywords.put("or", TokenType.OR);
+        keywords.put("print", TokenType.PRINT);
         keywords.put("return", TokenType.RETURN);
-        keywords.put("super",  TokenType.SUPER);
-        keywords.put("this",   TokenType.THIS);
-        keywords.put("true",   TokenType.TRUE);
-        keywords.put("var",    TokenType.VAR);
-        keywords.put("while",  TokenType.WHILE);
+        keywords.put("super", TokenType.SUPER);
+        keywords.put("this", TokenType.THIS);
+        keywords.put("true", TokenType.TRUE);
+        keywords.put("var", TokenType.VAR);
+        keywords.put("while", TokenType.WHILE);
     }
+
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
     private int start = 0;
@@ -40,6 +41,7 @@ public class Scanner {
 
     List<Token> scanTokens() {
         while (!isAtEnd()) {
+
             // We are at the beginning of the next lexeme.
             start = current;
             scanToken();
@@ -68,17 +70,38 @@ public class Scanner {
 
     private void scanToken() {
         char c = advance();
+
         switch (c) {
-            case '(': addToken(TokenType.LEFT_PAREN); break;
-            case ')': addToken(TokenType.RIGHT_PAREN); break;
-            case '{': addToken(TokenType.LEFT_BRACE); break;
-            case '}': addToken(TokenType.RIGHT_BRACE); break;
-            case ',': addToken(TokenType.COMMA); break;
-            case '.': addToken(TokenType.DOT); break;
-            case '-': addToken(TokenType.MINUS); break;
-            case '+': addToken(TokenType.PLUS); break;
-            case ';': addToken(TokenType.SEMICOLON); break;
-            case '*': addToken(TokenType.STAR); break;
+            case '(':
+                addToken(TokenType.LEFT_PAREN);
+                break;
+            case ')':
+                addToken(TokenType.RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(TokenType.LEFT_BRACE);
+                break;
+            case '}':
+                addToken(TokenType.RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(TokenType.COMMA);
+                break;
+            case '.':
+                addToken(TokenType.DOT);
+                break;
+            case '-':
+                addToken(TokenType.MINUS);
+                break;
+            case '+':
+                addToken(TokenType.PLUS);
+                break;
+            case ';':
+                addToken(TokenType.SEMICOLON);
+                break;
+            case '*':
+                addToken(TokenType.STAR);
+                break;
             case '!':
                 addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
                 break;
@@ -108,16 +131,16 @@ public class Scanner {
             case '\n':
                 line++;
                 break;
-            case '"': string(); break;
+            case '"':
+                string();
+                break;
 
             default:
                 if (isDigit(c)) {
                     number();
-                }
-         else if (isAlpha(c)) {
-            identifier();
-        }
-                else {
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
                     Lox.error(line, "Unexpected character.");
                 }
                 break;
@@ -184,10 +207,12 @@ public class Scanner {
         if (isAtEnd()) return '\0';
         return source.charAt(current);
     }
+
     private char peekNext() {
         if (current + 1 >= source.length()) return '\0';
         return source.charAt(current + 1);
     }
+
     private boolean isAlpha(char c) {
         return (c >= 'a' && c <= 'z') ||
                 (c >= 'A' && c <= 'Z') ||
