a35563331b8b5abb5e472846b95c06746511cde8
Ken Fowler <development@kennyf.ca>
2023-07-28 11:08:06 -0400

    feat: update ast printer

--- src/lox/AstPrinter.java
+++ src/lox/AstPrinter.java
@@ -1,6 +1,6 @@
 package lox;
 
-class AstPrinter implements Expr.Visitor<String> {
+class AstPrinter implements Expr.Visitor<String>, Stmt.Visitor<Void> {
     public static void main(String[] args) {
         Expr expr = new Expr.Binary(new Expr.Unary(
                 new Token(TokenType.MINUS, "-", null, 1),
@@ -14,24 +14,29 @@ class AstPrinter implements Expr.Visitor<String> {
     }
 
     @Override
-    public String visitBinaryExpression(Expr.Binary expression) {
+    public String visitBinaryExpr(Expr.Binary expression) {
         return parenthesize(expression.operator.lexeme, expression.left, expression.right);
     }
 
     @Override
-    public String visitGroupingExpression(Expr.Grouping expression) {
-        return parenthesize("group", expression.expr);
+    public String visitGroupingExpr(Expr.Grouping expression) {
+        return parenthesize("group", expression.expression);
     }
 
     @Override
-    public String visitLiteralExpression(Expr.Literal expression) {
+    public String visitLiteralExpr(Expr.Literal expression) {
         if (expression.value == null) return "nil";
         return expression.value.toString();
     }
 
     @Override
-    public String visitUnaryExpression(Expr.Unary expression) {
+    public String visitUnaryExpr(Expr.Unary expression) {
         return parenthesize(expression.operator.lexeme, expression.right);
+    }
+
+    @Override
+    public String visitVariableExpr(Expr.Variable expr) {
+        return null;
     }
 
     private String parenthesize(String name, Expr... exprs) {
@@ -47,4 +52,18 @@ class AstPrinter implements Expr.Visitor<String> {
         return builder.toString();
     }
 
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        return null;
+    }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        return null;
+    }
 }
